39ca85dfa6af011876cde456179fb7d635d0012f
Andrew Segre <asegre18@cmc.edu>
2020-10-05 15:43:48 -0700

    controller functions added

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,20 +1,33 @@
-const notes = require('../model/notes');
+const fs = require('fs');
 module.exports = {
-  getAllNotes: (req, res) => {
-    res.json(notes);
+  getAllNotes: async (_req, res) => {
+    await fs.readFile('./db/db.json', 'utf-9', (e, data) => {
+      if (e) throw e;
+      res.send(JSON.parse(data));
+    });
   },
-  addNote: (req, res) => {
-    notes.push({
-      "id": 8,
-      "fweet": "I am coming from a post request!"
-    })
-    res.json(notes);
+  addNote: async (req, res) => {
+    const input = req.body;
+    let notesList = [];
+    await res.json(fs.readFile('./db/db.json', 'utf-8', (e, data) => {
+      if (e) throw e;
+      notesList = JSON.parse(data);
+      input.id = notesList.length + 1;
+      notesList.push(input);
+      console.log(notesList);
+
+      fs.writeFile('./db/db.json', JSON.stringify(notesList), (e) => {
+        if (e) throw e;
+      });
+    }));
   },
-    deleteNote: (req, res) => {
-        notes.pop({
-          "id": 8,
-          "fweet": "I am coming from a post request!"
-        })
-    res.json(notes);
+    deleteNote: async (req, res) => {
+        const inputId = res.req.params['id'];
+        console.log('DELETED ' + inputId);
+        const correctId = JSON.parse(data).filter(note => note.id !== inputId);
+
+        await fs.writeFile('./db/db.json', JSON.stringify(correctId), (e) => {
+          if (e) throw e;
+        });
   }
-}
+};
