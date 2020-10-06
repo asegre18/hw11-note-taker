9c65d0ecd5101f89813b0ce06b44db20e70be0a6
Andrew Segre <asegre18@cmc.edu>
2020-10-05 18:43:48 -0700

    htmlRoutes added

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
 const noteRoutes = require('./notes');
 router.use('/notes', noteRoutes);
+
+router.get('/notes', noteController.getAllNotes).then(() => {
+    try {
+        
+    } catch (e) {
+        
+    }
+})
+
 module.exports = router;
---
+++ routes/htmlRoutes/htmlRoutes.js
@@ -0,0 +1,11 @@
+let path = require('path');
+const router = require('express').Router();
+router.get('/notes', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('/index', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
