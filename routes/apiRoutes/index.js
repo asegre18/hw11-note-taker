cc3c4f81604c4166a1ec08f78802c2bcc7cdc8d6
Andrew Segre <asegre18@cmc.edu>
2020-10-06 18:47:52 -0700

    still connecting html to backend

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const noteRoutes = require('./notes');
 router.use('/notes', noteRoutes);
 
-router.get('/notes', noteController.getAllNotes).then(() => {
-    try {
-        
-    } catch (e) {
-        
-    }
-})
+// router.get('/notes', noteController.getAllNotes).then(() => {
+//     try {
+//         return noteRoutes;
+//     } catch (e) {
+//         return e;
+//     }
+// })
 
 module.exports = router;
