af33854864dcd13e1dbb6e3fa6e46549da3b1244
Andrew Segre <asegre18@cmc.edu>
2020-12-18 17:33:33 -0800

    deploying

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv')
+  .config();
 const express = require('express');
 const routes = require('./routes');
 
@@ -6,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-app.listen(3000, () => console.log(`server started at http://localhost:3000`));
+app.listen(process.env.PORT, () => console.log(`server started at http://localhost:3000`));
 
 app.use('/', express.static('./public'));
 
